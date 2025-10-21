394c981617b1fd5b894cf54dca9f256d1681de50
ThisaraniWickramasinghe <thisaranidhammamali@gmail.com>
2025-10-21 17:37:50 +0530

    Created MedRecController

--- MobileBackend/haelin/src/main/java/com/haelin/controller/MedRecController.java
+++ MobileBackend/haelin/src/main/java/com/haelin/controller/MedRecController.java
@@ -1,4 +1,38 @@
 package com.haelin.controller;
 
+
+import com.haelin.model.MedRec;
+import com.haelin.service.MedRecService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/medrec")
 public class MedRecController {
+
+    @Autowired
+    private MedRecService medRecService;
+
+    @PostMapping
+    public String createOrUpdateRecord(@RequestBody MedRec medRec) throws ExecutionException, InterruptedException {
+        return medRecService.saveRecord(medRec);
+    }
+
+    @GetMapping
+    public List<MedRec> getAllRecords() throws ExecutionException, InterruptedException {
+        return medRecService.getAllRecords();
+    }
+
+    @GetMapping("/{id}")
+    public MedRec getRecordById(@PathVariable String id) throws ExecutionException, InterruptedException {
+        return medRecService.getRecordById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRecord(@PathVariable String id) {
+        return medRecService.deleteRecord(id);
+    }
 }
