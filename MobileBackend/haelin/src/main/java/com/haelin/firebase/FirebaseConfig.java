532c4acb9e96af78039cb6f588b8569140fea562
M H K Viduranga <kavinduv69@gmail.com>
2025-11-24 10:46:49 +0530

    Dockerized backend

--- MobileBackend/haelin/src/main/java/com/haelin/firebase/FirebaseConfig.java
+++ MobileBackend/haelin/src/main/java/com/haelin/firebase/FirebaseConfig.java
@@ -7,17 +7,19 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/static/haelinFirebaseKey.json");
+        ClassPathResource resource = new ClassPathResource("haelinFirebaseKey.json");
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
